c7a2f3b158ecd0f6c4959c8d03f5ce0b77cd4713
gnvenky <gnvenky@yahoo.com>
2026-01-19 22:43:35 -0600

    Update lib.rs

--- rust-refactored/src/lib.rs
+++ rust-refactored/src/lib.rs
@@ -1,1 +1,17 @@
+pub mod parser;
+
+#[cfg(test)]
+mod tests {
+    use super::parser::*;
+    
+    #[test]
+    fn test_parse_message() {
+        let data = [1u8, 0x00, 0x2A];
+        let result = parser::parse_message(&data);
+        assert!(result.is_ok());
+        let msg = result.unwrap();
+        assert_eq!(msg.id, 1);
+        assert_eq!(msg.value, 42);
+    }
+}
 // idiomatic Rust
