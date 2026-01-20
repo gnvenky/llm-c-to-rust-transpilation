95c69f196d71fd15e09086f203d775327e2ff30e
gnvenky <gnvenky@yahoo.com>
2026-01-19 22:39:35 -0600

    Update parser.h

--- legacy-c/include/parser.h
+++ legacy-c/include/parser.h
@@ -1,1 +1,20 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct {
+    uint8_t *data;
+    size_t len;
+} Buffer;
+
+typedef struct {
+    int id;
+    uint16_t value;
+} Message;
+
+int parse_message(Buffer *buf, Message *msg);
+
+#endif // PARSER_H
 // legacy C header
