1ef5bb9fffc696f7cba5604ebf12163f588a64a8
gnvenky <gnvenky@yahoo.com>
2026-01-19 22:39:57 -0600

    Update parser.c

--- legacy-c/src/parser.c
+++ legacy-c/src/parser.c
@@ -1,1 +1,11 @@
+#include "parser.h"
+#include <string.h>
+
+int parse_message(Buffer *buf, Message *msg) {
+    if (!buf || !msg || buf->len < sizeof(Message)) return -1;
+
+    msg->id = buf->data[0];
+    msg->value = (buf->data[1] << 8) | buf->data[2];
+    return 0;
+}
 // legacy C parser
