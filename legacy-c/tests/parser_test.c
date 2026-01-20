dcb3799ca3720dc49f01b5ce764e725f38c451cf
gnvenky <gnvenky@yahoo.com>
2026-01-19 22:41:12 -0600

    Update parser_test.c

--- legacy-c/tests/parser_test.c
+++ legacy-c/tests/parser_test.c
@@ -1,1 +1,18 @@
+#include "parser.h"
+#include <stdio.h>
+
+int main() {
+    uint8_t data[] = {1, 0x00, 0x2A};
+    Buffer buf = { data, sizeof(data) };
+    Message msg;
+    
+    int r = parse_message(&buf, &msg);
+    if (r == 0) {
+        printf("Parsed id=%d, value=%d\n", msg.id, msg.value);
+        return 0;
+    } else {
+        printf("Parse failed\n");
+        return 1;
+    }
+}
 
